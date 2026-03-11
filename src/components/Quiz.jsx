c45b8e56e032edae4686b43fa68a9813fdd63489
c-txr <chaden.teixeira78@gmail.com>
2026-03-11 15:55:34 +0100

    setup de la logique du quiz et ajout du fichier json

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export default function Quiz({ questions, onFinish }) {
+    /* La mémoire */
+    const [index, setIndex] = useState(0); /* commence à la première question */
+    const [score, setScore] = useState(0); /* commence à 0 pts */
+    const [fini, setFini] = useState(false); /* jeu terminé ? */
+
+    const currentQ = questions[index]; 
+
+    const handleAnswer = (selectedIndex) => {
+        if (selectedIndex === currentQ.correct) {
+            setScore(score +  50 ); /* si réponse correcte, + 50 pts */
+        }
+
+        if (index + 1 < questions.length){ /* vérif si il reste des questions */
+            setIndex(index +1);
+        } else {
+            setFini(true);
+        }
+    };
+
+    if (fini){
+        return <div>Bravo ! Score: {score} / {questions.length}</div>
+    }
+
+    return (
+        <div>  
+            <h2>{currentQ.question}</h2>
+            {currentQ.options.map((option, i) => (
+                <button key={i} onClick={() => handleAnswer(i)}>
+                    {option}
+                </button>
+            ))}
+        </div>
+    )
+ }
